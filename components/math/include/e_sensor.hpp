a9d778286a417619c61e04ffff59d98e02e681d2
thmalmeida <thmalmeida@gmail.com>
2023-12-17 22:12:38 -0300

    added channel parameter on constructor

--- components/math/include/e_sensor.hpp
+++ components/math/include/e_sensor.hpp
@@ -13,29 +13,35 @@
  *	- some DSP functions;
  *
  *
- * 
-*/
+ *
+ */
 
-class E_Sensor {
+class E_Sensor
+{
 public:
-	E_Sensor(ADC_driver *adc) {
+	E_Sensor(ADC_driver *adc, int channel) : channel_{channel}
+	{
 		init();
 	}
-	~E_Sensor(void) {
+	~E_Sensor(void)
+	{
 		delete[] i_k;
 		delete[] v_k;
 	}
 
-	void init(void) {
-		
+	void init(void)
+	{
+
 		// memory allocation for arrays
 		i_k = new int[length_];
 		i_t = new double[length_];
 
 		// configure adc channel
-		adc_.channel_config(1, 0, 12);
+		adc_->oneshot_channel_config(channel_, 0, 12);
+		// adc_.channel_config(1, 0, 12);
 	}
-	double i_rms(void) {
+	double i_rms(void)
+	{
 
 		// read analog samples and convert to digital
 		adc_.read(channel_, i_k, length_, sample_rate_);
@@ -45,28 +51,28 @@ public:
 
 		// remove offset component
 		dsp_.dc_remove(i_t, length_);
-		
+
 		// return rms value
-		return dsp_.calc_rms(i_t, length_);;
+		return dsp_.calc_rms(i_t, length_);
 	}
 	/* @brief Calculate the v_rms
-	* 
-	*/	
-	double v_rms(void) {   
-	// dsp_.
-
+	 *
+	 */
+	double v_rms(void)
+	{
+		// dsp_.
 	}
 
-	void set_channel(int channel) {
-
+	void set_channel(int channel)
+	{
 	}
 
 private:
-	void set_frequency_(int freq) {
-
+	void set_frequency_(int freq)
+	{
 	}
-	void set_length(int len) {
-		
+	void set_length(int len)
+	{
 	}
 
 	ADC_driver adc_;
@@ -82,10 +88,10 @@ private:
 }
 
 /* Sensors mapping
-* iSensor_1: GPIO36/ADC1_0
-* iSensor_2: GPIO39/ADC1_3
-* pSensor_1: GPIO32/ADC1_4
-* vSensor_R: GPIO34/ADC1_6
-* pSensor_2: GPIO35/ADC1_7
-*/
+ * iSensor_1: GPIO36/ADC1_0
+ * iSensor_2: GPIO39/ADC1_3
+ * pSensor_1: GPIO32/ADC1_4
+ * vSensor_R: GPIO34/ADC1_6
+ * pSensor_2: GPIO35/ADC1_7
+ */
 #endif
