61f0ad61e07719eb7e8d9db2aac00c080f93552e
thmalmeida <thmalmeida@gmail.com>
2023-12-18 21:25:04 -0300

    set freq, length and channel functios added

--- components/math/include/e_sensor.hpp
+++ components/math/include/e_sensor.hpp
@@ -19,32 +19,26 @@
 class E_Sensor
 {
 public:
-	E_Sensor(ADC_driver *adc, int channel) : channel_{channel}
-	{
+	E_Sensor(ADC_driver *adc, int channel) : adc_{adc}, channel_{channel} {
 		init();
 	}
-	~E_Sensor(void)
-	{
+	~E_Sensor(void)	{
 		delete[] i_k;
 		delete[] v_k;
 	}
 
-	void init(void)
-	{
-
+	void init(void) {
 		// memory allocation for arrays
-		i_k = new int[length_];
+		i_k = new uint16_t[length_];
 		i_t = new double[length_];
 
 		// configure adc channel
 		adc_->oneshot_channel_config(channel_, 0, 12);
 		// adc_.channel_config(1, 0, 12);
 	}
-	double i_rms(void)
-	{
-
+	double i_rms(void) {
 		// read analog samples and convert to digital
-		adc_.read(channel_, i_k, length_, sample_rate_);
+		adc_->read(channel_, i_k, length_, sampling_rate_);
 
 		// convert to current signal based on electric parameters configured on dsp class
 		dsp_.calc_iL_t(i_k, i_t, length_);
@@ -58,31 +52,32 @@ public:
 	/* @brief Calculate the v_rms
 	 *
 	 */
-	double v_rms(void)
-	{
+	double v_rms(void) {
 		// dsp_.
+		return 0;
 	}
 
-	void set_channel(int channel)
-	{
+	void set_channel(int channel) {
+		channel_ = channel;
 	}
 
 private:
-	void set_frequency_(int freq)
-	{
+	void set_frequency_(int Fs) {
+		sampling_rate_ = Fs;
 	}
-	void set_length(int len)
-	{
+	void set_length_(int len) {
+		length_ = len;
 	}
 
-	ADC_driver adc_;
+	ADC_driver *adc_;
 	DSP dsp_;
 
 	double *i_t;
-	int *i_k;
+	uint16_t *i_k;
 	int *v_k;
+
 	int length_ = 100;
-	int sample_rate_ = 1000;
+	int sampling_rate_ = 1000;
 
 	int channel_;
 }
